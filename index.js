793021c7810aea8ac25047d9904bd4461fda630f
Johan <johan@jeksn.me>
2024-11-23 20:25:27 +0100

    Fix responsive and add new accordion component

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
 
-
+const currentYearSpan = document.getElementById('currentYear');
+const date = new Date();
+currentYearSpan.textContent = date.getFullYear();
